c22db3577dc9d1d57284fbffa6188b2d82ee1fdb
vital <vitaliy_serdyukov@hotmail.com>
2021-10-08 13:38:12 +0200

    first commit

---
+++ src/main/java/dk/vsa/vsa_dat21v1/HomeController.java
@@ -0,0 +1,19 @@
+package dk.vsa.vsa_dat21v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String index(){
+    return "index";
+  }
+
+  @GetMapping("/hejhej")
+  public String greeting(){
+      return "hej";
+  }
+}
---
+++ src/main/java/dk/vsa/vsa_dat21v1/VsaDat21v1Application.java
@@ -0,0 +1,13 @@
+package dk.vsa.vsa_dat21v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsaDat21v1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VsaDat21v1Application.class, args);
+  }
+
+}
